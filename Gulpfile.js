37cb8a2c0276f0242f8628c26f5bd8a7416fc267
Rob McLaughlin IV <rob.mclaughlin@creditcards.com>
2024-07-20 01:48:36 -0500

    updated gulp file and package.json;

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,25 +1,39 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('img', () => gulp.src(['assets/img/**/*']).pipe(gulp.dest('./public/img')))
+// Define the 'img' task
+function imgTask() {
+  return gulp.src('assets/img/**/*')
+    .pipe(gulp.dest('./public/img'));
+}
 
-gulp.task('js', () => gulp.src(['assets/js/**/*']).pipe(gulp.dest('./public/js')))
+// Define the 'js' task
+function jsTask() {
+  return gulp.src('assets/js/**/*')
+    .pipe(gulp.dest('./public/js'));
+}
 
-gulp.task('css', () => {
-    let config = {
-        outputStyle: 'compressed'
-    }
-    return  gulp.src(['assets/sass/**/*.scss'])
-                .pipe(
-                    sass(config).on('error', sass.logError)
-                )
-                .pipe(
-                    gulp.dest('./public/css')
-                )
-})
+// Define the 'css' task
+function cssTask() {
+  const config = {
+    outputStyle: 'compressed'
+  };
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass(config).on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+}
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./assets/sass/**/*.scss', ['css']);
-})
+// Define the 'watch' task for SASS
+function watchTask() {
+  gulp.watch('./assets/sass/**/*.scss', cssTask);
+}
 
-gulp.task('default', ['css','img','js'])
+// Define the default task
+const build = gulp.series(gulp.parallel(cssTask, imgTask, jsTask));
+
+// Export tasks to be run with gulp
+exports.img = imgTask;
+exports.js = jsTask;
+exports.css = cssTask;
+exports.watch = watchTask;
+exports.default = build;
